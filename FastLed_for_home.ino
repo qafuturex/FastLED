14ea048b36b1a3fe950b45b1a12ba2c0ea87a496
anvar.sqa <anvar.sqa@gmail.com>
2021-02-07 22:13:23 +0300

    added comments

---
+++ FastLed_for_home.ino
@@ -0,0 +1,19 @@
+#define NUM_LEDS 50
+#include "FastLED.h"
+#define PIN 10
+CRGB leds[NUM_LEDS];
+byte counter;
+void setup() {
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(200);
+  pinMode(13, OUTPUT);
+}
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
+    leds[i] = CHSV(counter + i * 3, 255, 255);  // HSV. Увеличивать HUE (цвет)
+    // умножение i уменьшает шаг радуги
+  }
+  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  FastLED.show();
+  delay(7);         // скорость движения радуги
+}
